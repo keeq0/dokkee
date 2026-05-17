930e48bc2e0487be862d65563ed111089c51f1eb
keeq0 <myakotnykh1689@gmail.com>
2026-05-17 16:25:36 +0300

    feat: добавил LoginPage placeholder (#1)

---
+++ frontend/src/views/auth/LoginPage.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="login-placeholder">
+    <h1>Вход</h1>
+    <p>Страница входа появится в PR-5.</p>
+    <router-link to="/">На главную</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoginPage'
+}
+</script>
+
+<style scoped>
+.login-placeholder {
+  max-width: 480px;
+  margin: 100px auto;
+  padding: 40px;
+  text-align: center;
+  font-family: 'Montserrat', sans-serif;
+}
+
+.login-placeholder h1 {
+  margin-bottom: 16px;
+  color: #6C67FD;
+}
+
+.login-placeholder p {
+  margin-bottom: 24px;
+  color: #444;
+}
+
+.login-placeholder a {
+  color: #6C67FD;
+  text-decoration: underline;
+}
+</style>
