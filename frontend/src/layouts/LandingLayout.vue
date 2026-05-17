600f1362ffafd8ffd3b78b0d0c9797198b2f5f1e
keeq0 <myakotnykh1689@gmail.com>
2026-05-17 16:24:46 +0300

    feat: добавил LandingLayout (#1)

---
+++ frontend/src/layouts/LandingLayout.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="landing-layout">
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LandingLayout'
+}
+</script>
+
+<style scoped>
+.landing-layout {
+  min-height: 100vh;
+}
+</style>
